0460494233d057cb1c4ba23608fa0b6898855185
Anthony J Burton <anthony.burton@venmo.com>
2014-07-30 09:30:34 -0500

    updates scripts to work with virtualbox and first pass at debian

--- examples/multinode/11_setup_havana.sh
+++ examples/multinode/11_setup_havana.sh
@@ -4,4 +4,4 @@ vagrant ssh puppet -c "sudo ln -s /openstack /etc/puppet/modules; \
 sudo ln -s /openstack/examples/hiera.yaml /etc/puppet/hiera.yaml; \
 sudo mkdir -p /etc/puppet/hieradata; \
 sudo ln -s /openstack/examples/common.yaml /etc/puppet/hieradata/common.yaml; \
-sudo service puppetmaster restart;"
+sudo /etc/init.d/puppetmaster restart;"
